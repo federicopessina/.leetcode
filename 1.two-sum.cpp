219bbfb31742b5a3c06e87022ac18513e901d06a
federicopessina <federicopessina.work@gmail.com>
2026-05-14 12:01:59 +0200

    feat: two-sum

---
+++ 1.two-sum.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+
+            if (seen.count(complement)) {
+                return {seen[complement], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+// @lc code=end
+
